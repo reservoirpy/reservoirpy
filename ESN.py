6459e7809b649f960f52fa5814bf0668b7c3a956
jokfun <31824574+jokfun@users.noreply.github.com>
2019-02-19 09:52:19 +0100

    Update ESN.py

--- ESN.py
+++ ESN.py
@@ -11,26 +11,42 @@ http://minds.jacobs-university.de/mantas/code
 
 import numpy as np
 from scipy import linalg
+import sklearn.linear_model as sklm
 
 class ESN():
-    def __init__(self, lr, W, Win, input_bias=True, ridge=None, Wfb=None, fbfunc=None):
+    def __init__(self, lr, W, Win, input_bias=True, ridge=None, Wfb=None, fbfunc=None, typefloat=np.float64,reg_model=None):
         #TODO : add check if fbfunc is not set when Wfb is not None
         """
         Dimensions of matrices:
             - W : (nr_neurons, nr_neurons)
             - Win : (nr_neurons, dim_input)
             - Wout : (dim_output, nr_neurons)
+            
+        Inputs:                        
+            - reg_model: sklearn.linear_model regression object.
+                If it is defined, the provided model is used to learn output weights. 
+                ridge coefficient must not be defined at the same time
+                Examples : reg_model = sklearn.linear_model.Lasso(1e-8)
+                            reg_model = sklearn.linear_model.Ridge(1e-8)
+                            
         """
-        self.lr = lr # leaking rate
         self.W = W # reservoir matrix
         self.Win = Win # input matrix
+        
+        self.typefloat = typefloat
+        self.lr = lr # leaking rate
+        
         self.Wout = None
         self.Wfb = Wfb
         self.fbfunc = fbfunc
         self.N = self.W.shape[1] # nr of neurons
         self.in_bias = input_bias
         self.dim_inp = self.Win.shape[1] # dimension of inputs (including the bias at 1)
+        
         self.ridge = ridge
+        self.reg_model = None
+        self.update_regression_model(ridge, reg_model)
+        
         if self.in_bias:
             pass
         else:
@@ -42,7 +58,19 @@ class ESN():
         else:
             self.dim_out = None
         self.autocheck_nan()
-
+    
+    def update_regression_model(self, ridge = None, reg_model = None):
+        
+        if ridge is not None and reg_model is not None:
+            raise Exception( "ridge and reg_model can't be defined at the same time")
+        
+        if ridge is not None:
+            self.ridge = ridge
+            self.reg_model = None           
+            
+        if reg_model is not None:
+            self.reg_model = reg_model
+            self.ridge = None
 
     def autocheck_nan(self):
         """ Auto-check to see if some important variables do not have a problem (e.g. NAN values). """
@@ -88,9 +116,9 @@ class ESN():
                 y = np.zeros((self.dim_out,1)) # I guess it's the best we can do, because we have no info on the teacher for this time step
                 assert teachers[0].shape[1] == self.dim_out # check if output dimension is correct
 
-    def train(self, inputs, teachers, wash_nr_time_step, reset_state=True, float32=False, verbose=False):
+    def train(self, inputs, teachers, wash_nr_time_step, reset_state=True, verbose=False):
         #TODO float32 : use float32 precision for training the reservoir instead of the default float64 precision
-        #TODO: add a 'speed mode' where all asserts, prints and saved values are minimal
+        #TODO: add a 'speed mode' where all asserts, print(s and saved values are minimal
         #TODO: add option to enable direct connection from input to output to be learned
             # need to remember the input at this stage
         """
@@ -106,7 +134,7 @@ class ESN():
         - TODO float32 : use float32 precision for training the reservoir instead of the default float64 precision
         - TODO: add option to enable direct connection from input to output to be learned
             # need to remember the input at this stage
-        - TODO: add a 'speed mode' where all asserts, prints and saved values are minimal
+        - TODO: add a 'speed mode' where all asserts, print(s and saved values are minimal
         """
         if verbose:
             print( "len(inputs)", len(inputs))
@@ -125,12 +153,10 @@ class ESN():
 
         # change of variable for conveniance in equation
         di = self.dim_inp
-
-        if float32:
             #TODO: FINISH TO PUT ALL USEFUL VARIABLES IN float32
-            inputs = [aa.astype('float32') for aa in inputs]
-            teachers = [aa.astype('float32') for aa in teachers]
-            raise Exception( "TODO: float32 option not finished yet!")
+        inputs = [aa.astype(self.typefloat) for aa in inputs]
+        teachers = [aa.astype(self.typefloat) for aa in teachers]
+            #raise Exception( "TODO: float32 option not finished yet!")
 
         # run reservoir over the inputs, to save the internal states and the teachers (desired outputs) in lists
         for (j, (inp, tea)) in enumerate(zip(inputs, teachers)):
@@ -143,17 +169,19 @@ class ESN():
                 u = np.column_stack((np.ones((inp.shape[0],1)),inp))
             else:
                 u = inp
+            u = u.astype(self.typefloat)
             # reset the states or not, that is the question
             if reset_state:
-                x = np.zeros((self.N,1)) # current internal state initialized at zero
+                x = np.zeros((self.N,1),dtype=self.typefloat) # current internal state initialized at zero
                 if self.Wfb is not None:
-                    y = np.zeros((self.dim_out,1))
+                    y = np.zeros((self.dim_out,1),dtype=self.typefloat)
+
             else:
                 # keep the previously runned state
                 pass
 
-            all_int_states[j] = np.zeros((self.N,inp.shape[0]-wash_nr_time_step))
-            all_teachers[j] = np.zeros((tea.shape[1],inp.shape[0]-wash_nr_time_step))
+            all_int_states[j] = np.zeros((self.N,inp.shape[0]-wash_nr_time_step),dtype=self.typefloat)
+            all_teachers[j] = np.zeros((tea.shape[1],inp.shape[0]-wash_nr_time_step),dtype=self.typefloat)
 
             for t in range(inp.shape[0]): # for each time step in the input
                 # u = data[t]
@@ -195,15 +223,15 @@ class ESN():
                         print( "(1-self.lr) * x", (1-self.lr) * x)
                         print( "np.dot( self.Win, u[t,:].reshape(di,1) )", np.dot( self.Win, u[t,:].reshape(di,1) ))
                         print( "np.dot( self.W, x )", np.dot( self.W, x ))
-                    x = (1-self.lr) * x  +  self.lr * np.tanh( np.dot( self.Win, u[t,:].reshape(di,1) ) + np.dot( self.W, x ) )
+                    x = ((1-self.lr) * x  +  self.lr * np.tanh( np.dot( self.Win, u[t,:].reshape(di,1) ) + np.dot( self.W, x ) )).astype(self.typefloat)
                     if verbose:
                         print( "DEBUG AFTER")
                         print( "x.shape", x.shape)
                         print( "x", x)
                     # raw_input()
                 else:
-                    x = (1-self.lr) * x  +  self.lr * np.tanh( np.dot( self.Win, u[t,:].reshape(di,1) ) + np.dot( self.W, x ) + np.dot( self.Wfb, self.fbfunc(y) ) )
-                    y = tea[t,:].reshape(self.dim_out,1)
+                    x = ((1-self.lr) * x  +  self.lr * np.tanh( np.dot( self.Win, u[t,:].reshape(di,1) ) + np.dot( self.W, x ) + np.dot( self.Wfb, self.fbfunc(y) ) )).astype(self.typefloat)
+                    y = tea[t,:].reshape(self.dim_out,1).astype(self.typefloat)
 
 
                 if t >= wash_nr_time_step:
@@ -229,8 +257,8 @@ class ESN():
                         print( "x.reshape(-1,)", x.reshape(-1,))
                     #TODO: add option to enable direct connection from input to output to be learned
                         # need to remember the input at this stage
-                    all_int_states[j][:,t-wash_nr_time_step] = x.reshape(-1,)
-                    all_teachers[j][:,t-wash_nr_time_step] = tea[t,:].reshape(-1,)
+                    all_int_states[j][:,t-wash_nr_time_step] = x.reshape(-1,).astype(self.typefloat)
+                    all_teachers[j][:,t-wash_nr_time_step] = tea[t,:].reshape(-1,).astype(self.typefloat)
 
         if verbose:
             print( "all_int_states", all_int_states)
@@ -246,13 +274,13 @@ class ESN():
         self.check_values(array_or_list=all_teachers, value=None)
 
         # concatenate the lists
-        X = np.hstack(all_int_states)
-        Y = np.hstack(all_teachers)
+        X = np.hstack(all_int_states).astype(self.typefloat)
+        Y = np.hstack(all_teachers).astype(self.typefloat)
         if verbose:
             print( "X.shape", X.shape)
             print( "Y.shape", Y.shape)
         # Adding ones for regression with biais b in (y = a*x + b)
-        X = np.vstack((np.ones((1,X.shape[1])),X))
+        X = np.vstack((np.ones((1,X.shape[1]),dtype=self.typefloat),X))
         if verbose:
             print( "X.shape", X.shape)
         # raw_input()
@@ -263,7 +291,12 @@ class ESN():
         if verbose:
             print( "X_T.shape", X_T.shape)
             print( "Y.shape", Y.shape)
-        if self.ridge is not None:
+            
+        if self.reg_model is not None:
+            # scikit learn linear models are used for interpolation
+            Wout = self._linear_model_solving(X, Y)
+            
+        elif self.ridge is not None:
             # use ridge regression (linear regression with regularization)
             if verbose:
                 print( "USING RIDGE REGRESSION")
@@ -272,8 +305,9 @@ class ESN():
                 print( "Y", Y.shape)
                 print( "N", self.N)
             # Wout = np.dot(np.dot(Yt,X_T), linalg.inv(np.dot(X,X_T) + \
+            ridgeid = (self.ridge*np.eye(1+self.N)).astype(self.typefloat)
             Wout = np.dot(np.dot(Y,X_T), linalg.inv(np.dot(X,X_T) + \
-                    self.ridge*np.eye(1+self.N) ) )
+                    ridgeid ) )
                     # self.ridge*np.eye(1+inSize+resSize) ) )
 
             ### Just if you want to try the difference between scipy.linalg and numpy.linalg which does not give the same results
@@ -295,7 +329,6 @@ class ESN():
         self.x = x
         y = all_teachers[-1][:,-1] #the last time step of the last teacher
         self.y = y #useful when we will have feedback
-
         if verbose:
             print( "Wout.shape", Wout.shape)
             print( "all_int_states[0].shape", all_int_states[0].shape)
@@ -303,7 +336,20 @@ class ESN():
         # return all_int_states
         return [st.T for st in all_int_states]
 
-
+    def _linear_model_solving(self, X, Ytarget):
+        """
+            Uses regression method provided during network instanciation to return W such as W * X ~= Ytarget
+            First row of X MUST be only ones.
+        """
+        # Learning of the model (first row of X, which contains only ones, is removed) 
+        self.reg_model.fit(X[1:, :].T, Ytarget.T)
+        
+        # linear_model provides Matrix A and Vector b such as A * X[1:, :] + b ~= Ytarget       
+        A = np.asmatrix(self.reg_model.coef_)
+        b = np.asmatrix(self.reg_model.intercept_).T
+        
+        # Then the matrix W = "[b | A]" statisfies "W * X ~= Ytarget"
+        return np.asarray(np.hstack([b, A]))
 
 
     def run(self, inputs, reset_state=True, verbose=False):
@@ -325,16 +371,16 @@ class ESN():
         self.autocheck_io(inputs=inputs)
 
         if reset_state:
-            x = np.zeros((self.N,1))
+            x = np.zeros((self.N,1),dtype=self.typefloat)
             if self.Wfb is not None:
-                y = np.zeros((self.dim_out,1))
+                y = np.zeros((self.dim_out,1),dtype=self.typefloat)
         else:
             # run the trained ESN in a generative mode. no need to initialize here,
-            # because x is initialized with training data and we continue from there.
+            # because x is initialized with training data and we continue from there
             x = self.x
             if self.Wfb is not None:
-                y = self.y
-
+                y = (self.y).astype(self.typefloat)
+        
         # change of variable for conveniance in equation
         di = self.dim_inp
 
@@ -348,14 +394,15 @@ class ESN():
                 u = np.column_stack((np.ones((inp.shape[0],1)),inp))
             else:
                 u = inp
+            u = u.astype(self.typefloat)
             # reset the states or not, that is the question
             if reset_state:
-                x = np.zeros((self.N,1)) # current internal state initialized at zero
+                x = np.zeros((self.N,1),dtype=self.typefloat)#.astype(self.typefloat) # current internal state initialized at zero
                 if self.Wfb is not None:
-                    y = np.zeros((self.dim_out,1))
+                    y = np.zeros((self.dim_out,1),dtype=self.typefloat)
 
-            all_int_states[j] = np.zeros((self.N,inp.shape[0]))
-            all_outputs[j] = np.zeros((self.Wout.shape[0],inp.shape[0]))
+            all_int_states[j] = np.zeros((self.N,inp.shape[0]),dtype=self.typefloat)
+            all_outputs[j] = np.zeros((self.Wout.shape[0],inp.shape[0]),dtype=self.typefloat)
 
             # out = np.zeros((self.Wout.shape[0],inp))
 
@@ -367,9 +414,19 @@ class ESN():
                     x = (1-self.lr) * x  +  self.lr * np.tanh( np.dot( self.Win, u[t,:].reshape(di,1) ) + np.dot( self.W, x ) )
                 else:
                     x = (1-self.lr) * x  +  self.lr * np.tanh( np.dot( self.Win, u[t,:].reshape(di,1) ) + np.dot( self.W, x ) + np.dot( self.Wfb, self.fbfunc(y) ) )
-
                 # y = np.dot( Wout, np.vstack((1,u,x)) )
-                y = np.dot( self.Wout, np.vstack((1,x)) )
+                
+                
+                y = np.dot( self.Wout, np.vstack((1,x)) ).astype(self.typefloat)
+                
+                """
+                print("x :",x.dtype) #float32
+                print("vstack : ",np.vstack((1,x)).dtype) #float64, can't change the dtype of vstack
+                print("wout : ",self.Wout.dtype) #float32
+                print("---->",y.dtype) #float64
+                """
+                
+                #we have to convert y
 
                 if verbose:
                     print( "x.shape", x.shape)
@@ -385,6 +442,7 @@ class ESN():
                     print( "all_outputs[j][:,t].shape", all_outputs[j][:,t].shape)
                 all_int_states[j][:,t] = x.reshape(-1,)
                 all_outputs[j][:,t] = y.reshape(-1,)
+                
 
         # saving the last state of the reservoir, in case we want to run the reservoir from the last state on
         self.x = x
