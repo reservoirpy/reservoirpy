a8ae7d9e0f6d4ad04e8b1a6fccd41314c683e372
TrungDinhT <dinhthanhtrung1996@gmail.com>
2020-06-07 16:27:54 +0200

    Allow to save multiple data files with same loss

--- reservoirpy/hyper/hypersearch.py
+++ reservoirpy/hyper/hypersearch.py
@@ -6,6 +6,7 @@ import json
 import warnings
 from os import path
 from functools import partial
+from glob import glob
 
 import numpy as np
 
@@ -111,7 +112,7 @@ def research(objective, dataset, config_path, report_path=None):
             returned_dict['start_time'] = start
             returned_dict['duration'] = duration
             
-            save_file = f"{returned_dict['loss']:.6f}_hyperopt_results_1call.json"
+            save_file = f"{returned_dict['loss']:.6f}_hyperopt_results"
             
         except Exception as e:
             raise e
@@ -123,11 +124,14 @@ def research(objective, dataset, config_path, report_path=None):
                 'error': str(e),
             }
             
-            save_file = f"ERR{start}_hyperopt_results_1call.json"
+            save_file = f"ERR{start}_hyperopt_results"
             
         try:
             json_dict = {'returned_dict': returned_dict, 'current_params': kwargs}
-            with open(path.join(report_path, save_file), "w+") as f:
+            save_file = path.join(report_path, save_file)
+            nb_save_file_with_same_loss = len(glob(f"{save_file}*"))
+            save_file = f"{save_file}_{nb_save_file_with_same_loss+1}call.json"
+            with open(save_file, "w+") as f:
                 json.dump(json_dict, f)
         except Exception as e:
             warnings.warn("Results of current simulation were NOT saved correctly to JSON file.")
