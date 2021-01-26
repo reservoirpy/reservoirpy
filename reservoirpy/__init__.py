bbac5d7f7aeb5c549721bc9008bec0b6e2ed2ebc
nTrouvain <nathan.trouvain@inria.fr>
2021-01-26 11:32:13 +0100

    fix: setup format and version management

--- reservoirpy/__init__.py
+++ reservoirpy/__init__.py
@@ -1,10 +1,19 @@
+from ._version import __version__
+
 from . import mat_gen
 from . import observables
 from . import regression_models
 from . import activationsfunc
 from . import hyper
+
 from .esn import ESN
 from .esn_online import ESNOnline
 from .utils import load
 
-__version__ = "0.2.1"
+__all__ = [
+    "__version__",
+    "ESN", "ESNOnline",
+    "mat_gen", "activationsfunc",
+    "observables", "regression_models",
+    "hyper", "load"
+]
--- setup.py
+++ setup.py
@@ -1,24 +1,78 @@
-import setuptools
+import os
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    name="reservoirpy",
-    version="0.2.1",
-    author="Xavier Hinaut",
-    author_email="xavier.hinaut@inria.fr",
-    description="A simple and flexible code for Reservoir Computing architectures like Echo State Networks.",
-    long_description=long_description,
+NAME = "reservoirpy"
+
+__version__ = None
+version_file = os.path.join("reservoirpy", "__init__.py")
+with open(version_file) as f:
+    exec(f.read())
+
+AUTHOR = "Xavier Hinaut"
+AUTHOR_EMAIL = "xavier.hinaut@inria.fr"
+
+MAINTAINERS = "Xavier Hinaut, Nathan Trouvain"
+MAINTAINERS_EMAIL = "xavier.hinaut@inria.fr, nathan.trouvain@inria.fr"
+
+DESCRIPTION = ("A simple and flexible code for Reservoir "
+               "Computing architectures like Echo State Networks.")
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+URL = "https://github.com/reservoirpy/reservoirpy"
+DOWNLOAD_URL = (f"{URL}/v{__version__}.tar.gz")
+
+INSTALL_REQUIRES = [
+    "tqdm>=4.43.0",
+    "joblib>=0.12",
+    "dill>=0.3.0"
+    "numpy>=1.15.0",
+    "scipy>=1.0.0",
+    "joblib>=0.12"
+    ]
+
+PROJECT_URLS = {
+    'Bug Tracker': 'https://github.com/reservoirpy/reservoirpy/issues',
+    'Documentation': 'https://github.com/reservoirpy/reservoirpy',
+    'Source Code': URL
+}
+
+setup(
+    name=NAME,
+    version=__version__,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=MAINTAINERS,
+    maintainer_email=MAINTAINERS_EMAIL,
+    description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/reservoirpy/reservoirpy",
-    download_url='https://github.com/reservoirpy/reservoirpy/archive/v0.2.1.tar.gz',
-    packages=setuptools.find_packages(),
+    url=URL,
+    project_urls=PROJECT_URLS,
+    download_url=DOWNLOAD_URL,
+    packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        ('Topic :: Scientific/Engineering :: '
+         'Artificial Intelligence'),
+        'Development Status :: 4  - Beta',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        ('Programming Language :: Python :: '
+         'Implementation :: PyPy')
     ],
     python_requires='>=3.6',
     install_requires=[
