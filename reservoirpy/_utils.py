b6345d8e1bfa015a0a932c69171232e1b705c739
nTrouvain <nathan.trouvain@inria.fr>
2021-04-20 17:33:40 +0200

    feat: update utils for Reservoir class

--- reservoirpy/_utils.py
+++ reservoirpy/_utils.py
@@ -1,14 +1,41 @@
 import os
 import time
 import json
+
 from typing import Sequence, Union, Any
 
 import dill
 import numpy as np
 
 from scipy import sparse
+from scipy.sparse import issparse
+from numpy.random import Generator, RandomState, MT19937, default_rng
 
 import reservoirpy
+
+from ._types import RandomSeed
+
+
+def _random_generator(seed: RandomSeed) -> Generator:
+    """
+
+    Returns
+    -------
+    Generator:
+        A `numpy.random.Generator` instance.
+    """
+    if isinstance(seed, Generator):
+        return seed
+    # provided to support legacy RandomState generator
+    # of Numpy. It is not the best thing to do however
+    # and recommend the user to keep using integer seeds
+    # and proper Numpŷ Generator API.
+    if isinstance(seed, RandomState):
+        mt19937 = MT19937()
+        mt19937.state = seed.get_state()
+        return Generator(mt19937)
+    else:
+        return default_rng(seed)
 
 
 def _check_values(array_or_list: Union[Sequence, np.ndarray], value: Any):
@@ -24,6 +51,112 @@ def _check_values(array_or_list: Union[Sequence, np.ndarray], value: Any):
             # None is transformed to np.nan when it is in an array
             assert np.isnan(array_or_list).any() == False, \
                    f"{array_or_list} should not contain NaN values."
+
+
+def _format_data(X, y=None):
+
+    newX, newy = X, y
+
+    if isinstance(X, np.ndarray):
+        _check_array(X)
+        if X.ndim > 3:
+            raise ValueError(f"Found array with {X.ndim} dimensions. "
+                             "Input data must be of shape "
+                             "(n_samples, n_timesteps, n_features) "
+                             "or (n_timesteps, n_features).")
+        if X.ndim <= 2:
+            newX = [X]
+
+    elif isinstance(X, Sequence):
+        for i, x in enumerate(X):
+            if not(isinstance(x, np.ndarray)):
+                raise ValueError(f"Found object of type {type(x)} at index {i} "
+                                 "in data. All data samples must be Numpy arrays.")
+            else:
+                _check_array(x)
+
+    if y is not None:
+        if isinstance(y, np.ndarray):
+            if y.ndim > 3:
+                raise ValueError(f"Found array with {X.ndim} dimensions. "
+                                 "Input data must be of shape "
+                                 "(n_samples, n_timesteps, n_features) "
+                                 "or (n_timesteps, n_features).")
+            if y.ndim <= 2:
+                newy = [y]
+
+        elif isinstance(y, Sequence):
+            for i, yy in enumerate(y):
+                if not(isinstance(yy, np.ndarray)):
+                    raise ValueError(f"Found object of type {type(yy)} at index {i} "
+                                     "in targets. All targets must be Numpy arrays.")
+                else:
+                    _check_array(yy)
+
+    return newX, newy
+
+
+def _pack(arg):
+    if isinstance(arg, list):
+        if len(arg) >= 2:
+            return arg
+        else:
+            return arg[0]
+    else:
+        return arg
+
+
+def _check_array(array: np.ndarray):
+    """ Check if the given array or list contains the given value. """
+    if issparse(array):
+        _check_array(array.data)
+    else:
+        if not np.isfinite(np.asanyarray(array)).any():
+            num = np.sum(~np.isfinite(np.asarray(array)))
+            raise ValueError(f"Found {num} NaN or inf value in array : {array}.")
+
+        if isinstance(array, list):
+            if np.count_nonzero(np.asarray(array) == None) != 0:
+                raise ValueError(f"Found None in array : {array}")
+
+
+def _add_bias(vector, bias=1.0, pos="first"):
+    if pos == "first":
+        return np.hstack((np.ones((vector.shape[0], 1)) * bias, vector))
+    if pos == "last":
+        return np.hstack((vector, bias))
+
+
+def _check_vector(vector):
+
+    if not isinstance(vector, np.ndarray):
+        if issparse(vector):
+            checked_vect = vector
+        else:
+            try:
+                checked_vect = np.asanyarray(vector)
+            except Exception:
+                raise ValueError("Trying to use a data structure that is "
+                                 "not an array. Consider converting "
+                                 f"your data to numpy array format : {vector}")
+    else:
+        checked_vect = vector
+
+    if issparse(checked_vect):
+        pass
+    elif not np.issubdtype(checked_vect.dtype, np.number):
+        try:
+            checked_vect = checked_vect.astype(float)
+        except Exception:
+            raise ValueError("Trying to use non numeric data (of dtype "
+                             f"{vector.dtype}) : {vector}")
+
+    if checked_vect.ndim == 1:
+        checked_vect = checked_vect.reshape(1, -1)
+    elif checked_vect.ndim > 2:
+        raise ValueError("Trying to use data that is more than "
+                         f"2-dimensional ({vector.shape}): {vector}")
+    return checked_vect
 
 
 def _save(esn, directory: str):
@@ -108,12 +241,7 @@ def _save(esn, directory: str):
             "dim_out": dim_out,
             "typefloat": esn.typefloat.__name__,
             "reg_model": reg_model,
-            "fbfunc": fbfunc,
-            "noise_in": esn.noise_in,
-            "noise_out": esn.noise_out,
-            "noise_rc": esn.noise_rc,
-            "seed": esn.seed,
-            "sklearn_model": esn.sklearn_model
+            "fbfunc": fbfunc
         },
         "misc": {
             "fbfunc_info": fbfunc_info
@@ -153,7 +281,7 @@ def load(directory: str):
 
     model_attr = attr["attr"]
 
-    if os.path.splitext(model_attr["W"])[1] == ".npy":
+    if os.path.splitext(model_attr["W"])[1] == "npy":
         model_attr["W"] = np.load(os.path.join(directory, model_attr["W"]))
     else:
         model_attr["W"] = sparse.load_npz(os.path.join(directory, model_attr["W"]))
