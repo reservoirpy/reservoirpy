9e197fa1275b966f31c8ce17bab0c98c56115e2c
nTrouvain <nathan.trouvain@inria.fr>
2021-04-20 17:33:58 +0200

    feat: add custom types

---
+++ reservoirpy/_types.py
@@ -0,0 +1,23 @@
+from typing import Union, Callable, List, Tuple, Optional
+
+import numpy as np
+
+from scipy import sparse
+
+Weights = Union[np.ndarray,
+                sparse.csr_matrix,
+                sparse.csc_matrix,
+                sparse.coo_matrix]
+
+Numerical = Union[np.ndarray, int, float,
+                  List[Union[int, float]],
+                  Tuple[Union[int, float], ...]]
+
+Activation = Callable[[Numerical], Numerical]
+
+AnonymousReadout = Callable[[Numerical, Optional[Numerical]], Numerical]
+
+AnyReadout = Union[AnonymousReadout,
+                   Callable[..., Numerical]]
+
+RandomSeed = Union[np.random.Generator, np.random.RandomState, int]
