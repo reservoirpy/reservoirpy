bf75e0713775c7a966deaa45dfbdb30ed39e6f68
nTrouvain <nathan.trouvain@inria.fr>
2021-08-26 17:15:17 +0200

    feat: WIP:  Reservoir node first steps

--- reservoirpy/nodes/reservoir.py
+++ reservoirpy/nodes/reservoir.py
@@ -1,28 +1,30 @@
-from typing import Callable, Union, Optional
 from functools import partial
+from typing import Callable, Optional, Union
 
 import numpy as np
 from numpy.random import Generator, default_rng
 
-from ..utils.types import Weights
-from ..node import Node
-from ..mat_gen import generate_internal_weights, generate_input_weights
-from ..utils.validation import is_array
 from ..activationsfunc import identity, tanh
+from ..mat_gen import generate_input_weights, generate_internal_weights
+from ..node import Node
+from ..utils.types import Weights
+from ..utils.validation import is_array
 
 
 def forward(reservoir: "Reservoir", u: np.ndarray) -> np.ndarray:
-
     lr = reservoir.lr
     activation = reservoir.activation
     W = reservoir.W
     Win = reservoir.Win
+    bias = reservoir.bias
     noise_in = reservoir.noise_in
     noise_rc = reservoir.noise_rc
     rg = reservoir.random_generator
 
-    _x = W @ reservoir.state().T
-    _x += Win @ _noisify(u.reshape(-1, 1), noise_in, rg)
+    s = reservoir.state().T
+
+    _x = W @ s
+    _x += Win @ _noisify(u.reshape(-1, 1), noise_in, rg) + bias
 
     if reservoir.has_feedback:
         Wfb = reservoir.Wfb
@@ -32,7 +34,7 @@ def forward(reservoir: "Reservoir", u: np.ndarray) -> np.ndarray:
         _y = fb_activation(reservoir.feedback().reshape(-1, 1))
         _x += Wfb @ _noisify(_y, noise_out, rg)
 
-    x1 = (1 - lr) * reservoir.state().T + lr * _noisify(activation(_x), noise_rc, rg)
+    x1 = (1 - lr) * s + lr * _noisify(activation(_x), noise_rc, rg)
 
     return x1.T
 
@@ -47,7 +49,6 @@ def initialize(reservoir,
                Win_init=None,
                input_bias=None,
                seed=None):
-
     if x is not None:
         reservoir.set_input_dim(x.shape[1])
 
@@ -82,8 +83,10 @@ def initialize(reservoir,
             bias_msg = "+ 1 (bias)" if input_bias else ""
             if Win.shape[0] != x.shape[1] + bias_dim:
                 raise ValueError("Dimension mismatch between Win and input "
-                                 f"vector in {reservoir.name}: Win is {Win.shape} "
-                                 f"and input is {x.shape} ({x.shape[1]} {bias_msg} "
+                                 f"vector in {reservoir.name}: Win is "
+                                 f"{Win.shape} "
+                                 f"and input is {x.shape} ({x.shape[1]} "
+                                 f"{bias_msg} "
                                  f"!= {Win.shape[0] - bias_dim} {bias_msg})")
 
             if Win.shape[1] != out_dim:
@@ -95,7 +98,7 @@ def initialize(reservoir,
 
         elif callable(Win_init):
             Win = Win_init(N=reservoir.output_dim, dim_input=x.shape[1],
-                           input_bias=input_bias, input_scaling=input_scaling,
+                           input_bias=False, input_scaling=input_scaling,
                            proba=input_connectivity, seed=seed)
         else:
             raise ValueError(f"Data type {type(Win_init)} not "
@@ -106,6 +109,12 @@ def initialize(reservoir,
 
         reservoir.set_param("Win", Win)
 
+        if input_bias:
+            bias = np.ones((1, 1))
+        else:
+            bias = np.zeros((1, 1))
+        reservoir.set_param("bias", bias)
+
 
 def initialize_feedback(reservoir,
                         Wfb_init=None,
@@ -113,7 +122,6 @@ def initialize_feedback(reservoir,
                         fb_connectivity=None,
                         fb_dim: int = None,
                         seed=None):
-
     if reservoir.has_feedback:
         feedback = reservoir.feedback()
         fb_dim = feedback.shape[1]
@@ -128,13 +136,15 @@ def initialize_feedback(reservoir,
             Wfb = Wfb_init
             if not fb_dim == Wfb.shape[0]:
                 raise ValueError("Dimension mismatch between Wfb and feedback "
-                                 f"vector in {reservoir.name}: Wfb is {Wfb.shape} "
+                                 f"vector in {reservoir.name}: Wfb is "
+                                 f"{Wfb.shape} "
                                  f"and feedback is {(1, fb_dim)} "
                                  f"({fb_dim} != {Wfb.shape[0]})")
 
             if not Wfb.shape[1] == reservoir.output_dim:
                 raise ValueError(f"Dimension mismatch between Wfb and W in "
-                                 f"{reservoir.name}: Wfb is {Wfb.shape} and W is "
+                                 f"{reservoir.name}: Wfb is {Wfb.shape} and "
+                                 f"W is "
                                  f"{reservoir.W.shape} ({Wfb.shape[1]} "
                                  f"!= {reservoir.output_dim})")
 
@@ -186,43 +196,44 @@ class Reservoir(Node):
                  activation: Union[str, Callable] = tanh,
                  name=None,
                  seed=None):
-
         if units is None and not is_array(W):
-            raise ValueError("'units' parameter must not be None if 'W' parameter is not"
-                             "a matrix.")
+            raise ValueError(
+                "'units' parameter must not be None if 'W' parameter is not"
+                "a matrix.")
 
-        super(Reservoir, self).__init__(fb_initializer=partial(initialize_feedback,
-                                                               Wfb_init=Wfb,
-                                                               fb_scaling=fb_scaling,
-                                                               fb_connectivity=fb_connectivity,
-                                                               fb_dim=fb_dim,
-                                                               seed=seed),
-                                        params={"W": None, "Win": None, "Wfb": None, "bias": None},
-                                        hypers={"lr": lr,
-                                                "sr": sr,
-                                                "input_scaling": input_scaling,
-                                                "fb_scaling": fb_scaling,
-                                                "rc_connectivity": rc_connectivity,
-                                                "input_connectivity": input_connectivity,
-                                                "fb_connectivity": fb_connectivity,
-                                                "noise_in": noise_in,
-                                                "noise_rc": noise_rc,
-                                                "noise_out": noise_fb,
-                                                "activation": activation,
-                                                "fb_activation": fb_activation,
-                                                "units": units},
-                                        forward=forward,
-                                        initializer=partial(initialize,
-                                                            sr=sr,
-                                                            input_scaling=input_scaling,
-                                                            input_connectivity=input_connectivity,
-                                                            rc_connectivity=rc_connectivity,
-                                                            W_init=W,
-                                                            Win_init=Win,
-                                                            input_bias=input_bias,
-                                                            seed=seed),
-                                        output_dim=units,
-                                        name=name)
+        super(Reservoir, self).__init__(
+            fb_initializer=partial(initialize_feedback,
+                                   Wfb_init=Wfb,
+                                   fb_scaling=fb_scaling,
+                                   fb_connectivity=fb_connectivity,
+                                   fb_dim=fb_dim,
+                                   seed=seed),
+            params={"W": None, "Win": None, "Wfb": None, "bias": None},
+            hypers={"lr": lr,
+                    "sr": sr,
+                    "input_scaling": input_scaling,
+                    "fb_scaling": fb_scaling,
+                    "rc_connectivity": rc_connectivity,
+                    "input_connectivity": input_connectivity,
+                    "fb_connectivity": fb_connectivity,
+                    "noise_in": noise_in,
+                    "noise_rc": noise_rc,
+                    "noise_out": noise_fb,
+                    "activation": activation,
+                    "fb_activation": fb_activation,
+                    "units": units},
+            forward=forward,
+            initializer=partial(initialize,
+                                sr=sr,
+                                input_scaling=input_scaling,
+                                input_connectivity=input_connectivity,
+                                rc_connectivity=rc_connectivity,
+                                W_init=W,
+                                Win_init=Win,
+                                input_bias=input_bias,
+                                seed=seed),
+            output_dim=units,
+            name=name)
 
         self._seed = seed
         self._rg = np.random.default_rng(seed)
