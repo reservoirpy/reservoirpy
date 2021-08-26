945a329d9e9105af3e2eca63809dd54b65e97198
nTrouvain <nathan.trouvain@inria.fr>
2021-08-26 17:15:56 +0200

    feat: WIP: I/O nodes for inputs repartition and states probing

--- reservoirpy/nodes/io.py
+++ reservoirpy/nodes/io.py
@@ -4,18 +4,18 @@
 from ..node import Node
 
 
-def _input_initialize(input_node: "Node", x=None):
+def _io_initialize(io_node: "Node", x=None):
     if x is not None:
-        if input_node.input_dim is None:
-            input_node.set_input_dim(x.shape[1])
-            input_node.set_output_dim(x.shape[1])
+        if io_node.input_dim is None:
+            io_node.set_input_dim(x.shape[1])
+            io_node.set_output_dim(x.shape[1])
 
 
 class Input(Node):
 
     def __init__(self, input_dim=None, name=None):
         super(Input, self).__init__(forward=lambda inp, x: x,
-                                    initializer=_input_initialize,
+                                    initializer=_io_initialize,
                                     input_dim=input_dim,
                                     output_dim=input_dim,
                                     name=name)
@@ -25,5 +25,5 @@ class Output(Node):
 
     def __init__(self, name=None):
         super(Output, self).__init__(forward=lambda inp, x: x,
-                                     initializer=_input_initialize,
+                                     initializer=_io_initialize,
                                      name=name)
