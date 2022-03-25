399dc09f26ff5778b0236cf54c722fc13987af9a
nTrouvain <nathan.trouvain@inria.fr>
2022-03-25 17:10:15 +0100

    Fix: wrong handling of Python version for typing.Literal import

--- reservoirpy/observables.py
+++ reservoirpy/observables.py
@@ -19,7 +19,7 @@ Metrics and observables for Reservoir Computing:
 # Copyright: Xavier Hinaut (2018) <xavier.hinaut@inria.fr>
 import sys
 
-if sys.version_info <= (3, 7):
+if sys.version_info < (3, 8):
     from typing_extensions import Literal
 else:
     from typing import Literal
