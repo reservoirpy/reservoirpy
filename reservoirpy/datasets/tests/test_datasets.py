cec1c2f508af44d43940085b8316090018907bc3
nTrouvain <nathan.trouvain@inria.fr>
2022-05-17 10:32:00 +0200

    Tests: datasets

--- reservoirpy/datasets/tests/test_datasets.py
+++ reservoirpy/datasets/tests/test_datasets.py
@@ -1,9 +1,21 @@
+import os
+from contextlib import contextmanager
+
 import numpy as np
 import pytest
+from joblib import Memory
 from numpy.testing import assert_allclose
 
-from reservoirpy import datasets
+from reservoirpy import _TEMPDIR, datasets
 from reservoirpy.datasets import to_forecasting
+
+
+@contextmanager
+def no_cache():
+    """Disable caching temporarily when running tests"""
+    datasets._chaos.memory = Memory(location=None)
+    yield
+    datasets._chaos.memory = Memory(os.path.join(_TEMPDIR, "datasets"), verbose=0)
 
 
 @pytest.mark.parametrize(
@@ -14,17 +26,59 @@ from reservoirpy.datasets import to_forecasting
         datasets.lorenz,
         datasets.mackey_glass,
         datasets.multiscroll,
+        datasets.doublescroll,
         datasets.rabinovich_fabrikant,
         datasets.narma,
+        datasets.lorenz96,
+        datasets.rossler,
+        datasets.kuramoto_sivashinsky,
     ],
 )
 def test_generation(dataset_func):
-
-    timesteps = 2000
-    X = dataset_func(timesteps)
+    with no_cache():
+        timesteps = 2000
+        X = dataset_func(timesteps)
 
     assert isinstance(X, np.ndarray)
     assert len(X) == timesteps
+
+
+@pytest.mark.parametrize(
+    "dataset_func,kwargs,expected",
+    [
+        (datasets.logistic_map, {"r": -1}, ValueError),
+        (datasets.logistic_map, {"x0": 1}, ValueError),
+        (datasets.mackey_glass, {"seed": 1234}, None),
+        (datasets.mackey_glass, {"seed": None}, None),
+        (datasets.mackey_glass, {"tau": 0}, None),
+        (datasets.narma, {"seed": 1234}, None),
+        (datasets.lorenz96, {"N": 1}, ValueError),
+        (datasets.lorenz96, {"x0": [0.1, 0.2, 0.3, 0.4, 0.5], "N": 4}, ValueError),
+        (datasets.rossler, {"x0": [0.1, 0.2]}, ValueError),
+        (
+            datasets.kuramoto_sivashinsky,
+            {"x0": np.random.normal(size=129), "N": 128},
+            ValueError,
+        ),
+        (
+            datasets.kuramoto_sivashinsky,
+            {"x0": np.random.normal(size=128), "N": 128},
+            None,
+        ),
+    ],
+)
+def test_kwargs(dataset_func, kwargs, expected):
+
+    if expected is None:
+        timesteps = 2000
+        X = dataset_func(timesteps, **kwargs)
+
+        assert isinstance(X, np.ndarray)
+        assert len(X) == timesteps
+    else:
+        with pytest.raises(expected):
+            timesteps = 2000
+            dataset_func(timesteps, **kwargs)
 
 
 @pytest.mark.parametrize("dataset_func", [datasets.mackey_glass])
