8a247d2d55874d0bcba7dbc7e962f300f975a69c
albane <albane.arthuis@inria.fr>
2023-04-25 12:19:02 +0200

    rectified time series code

--- testing/time_series.py
+++ testing/time_series.py
@@ -13,18 +13,17 @@ Y_train = X[:, 1:51]
 reservoir = Reservoir(100, lr=0.5, sr=0.9)
 # ridge = Ridge(ridge=1e-7)
 ridge = ScikitNode(name="Ridge", alpha=1.0, tol=1e-3)
-
 esn_model = reservoir >> ridge
 
 esn_model = esn_model.fit(X_train, Y_train, warmup=10)
-Y_pred = esn_model.run(X[50:])
+Y_pred = esn_model.run(X[:, 50:])
 Y_pred = np.array(Y_pred)
 
 plt.figure(figsize=(10, 3))
 plt.title("A sine wave and its future.")
 plt.xlabel("$t$")
-plt.plot(Y_pred, label="Predicted sin(t+1)", color="blue")
-plt.plot(X[51:], label="Real sin(t+1)", color="red")
+plt.plot(Y_pred[0], label="Predicted sin(t+1)", color="blue")
+plt.plot(X[0, 51:], label="Real sin(t+1)", color="red")
 plt.legend()
 plt.show()
 
