9b20617730b9f425bcb7b172e522be85970b8303
Deepayan137 <deepayan137@gmail.com>
2023-07-20 11:00:10 +0200

    final push

--- reservoirpy/nodes/tests/test_sklearn_node_classification.py
+++ reservoirpy/nodes/tests/test_sklearn_node_classification.py
@@ -28,26 +28,26 @@ def test_sklearn_classification(linear_model):
 	assert pred.shape == real.shape
 
 
-# @pytest.mark.parametrize(
-# 	"linear_model",
-# 	[("LogisticRegression"), ("RidgeClassifier")]
-# )
-# def test_sklearn_esn_classification(linear_model):
-# 	readout = SklearnNode(method=linear_model)
-# 	reservoir = Reservoir(100)
-# 	esn = reservoir >> readout
-# 	from sklearn.datasets import make_classification
-# 	from sklearn.metrics import accuracy_score
-# 	X, y = make_classification(n_samples=250, n_features=6, 
-#                            n_classes=3, random_state=0, n_informative=3)
-# 	enocoder = TransformInputSklearn()
-# 	X, y = enocoder(X, y, task="regression")
-# 	X_train, y_train = X[:100], y[:100]
-# 	res = readout.fit(X_train, y_train)
-# 	pred = readout.run(X[100:])
-# 	real = y[100:]
-# 	decoder = TransformOutputSklearn()
-# 	pred, real = decoder(pred, real)
-# 	assert pred.shape == real.shape
+@pytest.mark.parametrize(
+	"linear_model",
+	[("LogisticRegression"), ("RidgeClassifier")]
+)
+def test_sklearn_esn_classification(linear_model):
+	readout = SklearnNode(method=linear_model)
+	reservoir = Reservoir(100)
+	esn = reservoir >> readout
+	from sklearn.datasets import make_classification
+	from sklearn.metrics import accuracy_score
+	X, y = make_classification(n_samples=250, n_features=6, 
+                           n_classes=3, random_state=0, n_informative=3)
+	enocoder = TransformInputSklearn()
+	X, y = enocoder(X, y, task="regression")
+	X_train, y_train = X[:100], y[:100]
+	res = readout.fit(X_train, y_train)
+	pred = readout.run(X[100:])
+	real = y[100:]
+	decoder = TransformOutputSklearn()
+	pred, real = decoder(pred, real)
+	assert pred.shape == real.shape
 
 
