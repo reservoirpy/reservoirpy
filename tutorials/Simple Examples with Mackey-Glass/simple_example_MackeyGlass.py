e56417e1a974787bc8e775debe01ddb8b361e331
PAUL BERNARD <Paul.Bernard@inria.fr>
2024-04-29 15:21:50 +0200

    Doc: refact MG simple example

--- tutorials/Simple Examples with Mackey-Glass/simple_example_MackeyGlass.py
+++ tutorials/Simple Examples with Mackey-Glass/simple_example_MackeyGlass.py
@@ -16,16 +16,18 @@ References:
 -----------
     .. [1] Mantas Lukoševičius personal website: https://mantas.info/
 """
+
 # Author: Xavier Hinaut <xavier.hinaut@inria.fr> and
 # Nathan Trouvain at 01/12/2021 <nathan.trouvain@inria.fr>
 # Licence: MIT License
 # Copyright: Xavier Hinaut (2018) <xavier.hinaut@inria.fr>
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 import reservoirpy as rpy
 from reservoirpy.datasets import mackey_glass
-from reservoirpy.nodes import Reservoir, Ridge
+from reservoirpy.nodes import Input, Reservoir, Ridge
 
 # Set a particular seed for the random generator (for example seed = 42)
 # NB: reservoir performances should be averaged across at least 30 random
@@ -34,65 +36,104 @@ from reservoirpy.nodes import Reservoir, Ridge
 SEED = 42
 VERBOSE = True
 
+# Dataset parameters
+TIMESTEPS = 10_000
+TAU = 17
+NORMALIZE = True
+
+TRAIN_SIZE = 2000
+TEST_SIZE = 2000
+HORIZON = 1  # horizon p of the forecast (predict X[t+p] from X[t])
+
+
+# ---- A look into Echo State Networks parameters ----
+
+# Input dimension
+INPUT_BIAS = True  # add a constant input to 1
+N_INPUTS = 1  # input dimension (optional, can be inferred at runtime)
+N_OUTPUTS = 1  # output dimension (optional, can be inferred at runtime)
+
+# Reservoir parameter
+UNITS = 300  # number of recurrent units
+LEAK_RATE = 0.3  # leaking rate (=1/time_constant_of_neurons)
+RHO = 1.25  # Scaling of recurrent matrix
+INPUT_SCALING = 1.0  # Scaling of input matrix
+
+# Connectivity
+# Connectivity defines the probability that two neurons in the
+# reservoir are being connected
+# (the two neurons can be a neuron and itself)
+RC_CONNECTIVITY = 0.2  # Connectivity of recurrent matrix W
+INPUT_CONNECTIVITY = 1.0  # Connectivity of input matrix
+FB_CONNECTIVITY = 1.0  # Connectivity of feedback matrix
+
+# Readout parameters
+REGULARIZATION_COEF = 1e-8
+WARMUP = 100
+
+# ---- Model architecture ----
+
+# Enable feedback
+FEEDBACK = False
+# Adds the input values to the regression along the reservoir states
+INPUT_TO_READOUT = False
+
 if __name__ == "__main__":
-
     rpy.set_seed(SEED)
-
     # Set verbosity of ReservoirPy objects to 0 (no use here)
     rpy.verbosity(0)
 
     # ---- Loading data ----
-    units = 10000
-    tau = 17
-    data = mackey_glass(10000, tau=tau)
-
-    normalize = True
+    data = mackey_glass(TIMESTEPS, tau=TAU)
 
     if VERBOSE:
         print("Data dimensions", data.shape)
-        print("max:", data.max(), "min:", data.min())
-        print("mean:", data.mean(), "std:", data.std())
+        print(f"max: {data.max()} min: {data.min()}")
+        print(f"mean: {data.mean()}, std: {data.std()}")
 
     # Normalize data
-    if normalize:
+    if NORMALIZE:
         data = (data - data.min()) / (data.max() - data.min())
         if VERBOSE:
             print("Normalization...")
-            print("max:", data.max(), "min:", data.min())
-            print("mean:", data.mean(), "std:", data.std())
+            print(f"max: {data.max()} min: {data.min()}")
+            print(f"mean: {data.mean()}, std: {data.std()}")
 
     plt.figure()
     plt.plot(data[:1000])
     plt.title("A sample of input data")
     plt.show()
 
-    # ---- A look into Echo State Networks parameters ----
+    # ---- Prepare dataset ----
 
-    # Input dimension
-    input_bias = True  # add a constant input to 1
-    n_inputs = 1  # input dimension (optional, can be inferred at runtime)
-    n_outputs = 1  # output dimension (optional, can be inferred at runtime)
+    X = data[:TRAIN_SIZE]
+    y = data[HORIZON : TRAIN_SIZE + HORIZON]
 
-    # Reservoir parameter
-    units = 300  # number of recurrent units
-    leak_rate = 0.3  # leaking rate (=1/time_constant_of_neurons)
-    rho = 1.25  # Scaling of recurrent matrix
-    input_scaling = 1.0  # Scaling of input matrix
+    X_test = data[TRAIN_SIZE : TRAIN_SIZE + TEST_SIZE]
+    y_test = data[TRAIN_SIZE + HORIZON : TRAIN_SIZE + TEST_SIZE + HORIZON]
 
-    # Connectivity
-    # Connectivity defines the probability that two neurons in the
-    # reservoir are being connected
-    # (the two neurons can be a neuron and itself)
-    rc_connectivity = 0.2  # Connectivity of recurrent matrix W
-    input_connectivity = 1.0  # Connectivity of input matrix
-    fb_connectivity = 1.0  # Connectivity of feedback matrix
+    if VERBOSE:
+        print("X, y dimensions", X.shape, y.shape)
+        print("X_test, y_test dimensions", X_test.shape, y_test.shape)
 
-    # Readout parameters
-    regularization_coef = 1e-8
-    warmup = 100
+    plt.figure()
+    plt.plot(X, y)
+    plt.xlabel("$X[t]$")
+    plt.ylabel("$y[t]=X[t+p]$")
+    if NORMALIZE:
+        plt.ylim([-1.1, 1.1])
+    plt.title("Recurrence plot of training data: input(t+1) vs. input(t)")
+    plt.show()
 
-    # Enable feedback
-    feedback = False
+    plt.figure()
+    plt.plot(X, label="X")
+    plt.plot(y, label="y")
+    plt.xlabel("t")
+    if NORMALIZE:
+        plt.ylim([-1.1, 1.1])
+    plt.legend()
+    plt.title("$X[t] and y[t]=X[t+p]$")
+    plt.show()
 
     # ---- Generating random weight matrices with toolbox methods ----
     # (this is optional)
@@ -100,45 +141,56 @@ if __name__ == "__main__":
     # !! uncomment if you want to test the more
     # detailed matrix generation method !!
 
-    # from reservoirpy import mat_gen
-    # W = mat_gen.generate_internal_weights(units, spectral_radius=rho,
-    #                                       proba=rc_connectivity, seed=SEED)
-    #
-    # Win = mat_gen.generate_input_weights(units, n_inputs,
-    #                                      input_scaling=input_scaling,
-    #                                      proba=input_connectivity,
-    #                                      input_bias=input_bias,
-    #                                      seed=SEED)
-    #
-    # Wfb = mat_gen.generate_input_weights(units, n_outputs,
-    #                                      proba=fb_connectivity,
-    #                                      input_bias=False,
-    #                                      seed=SEED)
+    from reservoirpy import mat_gen
+
+    W = mat_gen.normal(
+        UNITS,
+        connectivity=RC_CONNECTIVITY,
+        sr=RHO,
+        seed=SEED,
+    )
+
+    Win = mat_gen.bernoulli(
+        UNITS,
+        N_INPUTS,
+        input_scaling=INPUT_SCALING,
+        connectivity=INPUT_CONNECTIVITY,
+        input_bias=INPUT_BIAS,
+        seed=SEED,
+    )
+
+    Wfb = mat_gen.bernoulli(
+        UNITS,
+        N_OUTPUTS,
+        connectivity=FB_CONNECTIVITY,
+        input_bias=False,
+        seed=SEED,
+    )
 
     # ---- Generating random weight matrices with custom method ----
     # (this is also optional)
 
     rng = np.random.default_rng(SEED)
 
-    W = rng.random((units, units)) - 0.5
-    Win = rng.random((units, n_inputs + int(input_bias))) - 0.5
-    Wfb = rng.random((units, n_outputs)) - 0.5
+    W = rng.random((UNITS, UNITS)) - 0.5
+    Win = rng.random((UNITS, N_INPUTS + int(INPUT_BIAS))) - 0.5
+    Wfb = rng.random((UNITS, N_OUTPUTS)) - 0.5
 
     # Delete the fraction of connections given the connectivity
     # (i.e. proba of non-zero connections in the reservoir):
-    mask = rng.random((units, units))  # create a mask Uniform[0;1]
-    W[mask > rc_connectivity] = 0  # set to zero some connections
+    mask = rng.random((UNITS, UNITS))  # create a mask Uniform[0;1]
+    W[mask > RC_CONNECTIVITY] = 0  # set to zero some connections
 
-    mask = rng.random((units, Win.shape[1]))
-    Win[mask > input_connectivity] = 0
+    mask = rng.random((UNITS, Win.shape[1]))
+    Win[mask > INPUT_CONNECTIVITY] = 0
 
-    mask = rng.random((units, Wfb.shape[1]))
-    Wfb[mask > fb_connectivity] = 0
+    mask = rng.random((UNITS, Wfb.shape[1]))
+    Wfb[mask > FB_CONNECTIVITY] = 0
 
     # Scaling of matrices
 
     # Scaling of input matrix
-    Win = Win * input_scaling
+    Win = INPUT_SCALING * Win
 
     # Scaling of recurrent matrix using a specific spectral radius
     # First compute the spectral radius of these weights:
@@ -156,83 +208,48 @@ if __name__ == "__main__":
         print("Default spectral radius before scaling:", original_spectral_radius)
 
     # Rescale them to reach the requested spectral radius:
-    W = W * (rho / original_spectral_radius)
+    W = W * (RHO / original_spectral_radius)
 
     if VERBOSE:
         print("Spectral radius after scaling:", spectral_radius(W))
-
-    # ---- Prepare dataset ----
-
-    train_size = 2000
-    test_size = 2000
-    horizon = 1  # horizon p of the forecast (predict X[t+p] from X[t])
-
-    X = data[:train_size]
-    y = data[horizon : train_size + horizon]
-
-    X_test = data[train_size : train_size + test_size]
-    y_test = data[train_size + horizon : train_size + test_size + horizon]
-
-    if VERBOSE:
-        print("X, y dimensions", X.shape, y.shape)
-        print("X_test, y_test dimensions", X_test.shape, y_test.shape)
-
-    plt.figure()
-    plt.plot(X, y)
-    plt.xlabel("$X[t]$")
-    plt.ylabel("$y[t]=X[t+p]$")
-    if normalize:
-        plt.ylim([-1.1, 1.1])
-    plt.title("Recurrence plot of training data: input(t+1) vs. input(t)")
-    plt.show()
-
-    plt.figure()
-    plt.plot(X, label="X")
-    plt.plot(y, label="y")
-    plt.xlabel("t")
-    if normalize:
-        plt.ylim([-1.1, 1.1])
-    plt.legend()
-    plt.title("$X[t] and y[t]=X[t+p]$")
-    plt.show()
 
     # ---- Create an Echo State Network ----
 
     # Create a reservoir
     reservoir = Reservoir(
-        units,
-        lr=leak_rate,
-        sr=rho,
-        input_bias=input_bias,
-        input_scaling=input_scaling,
-        rc_connectivity=rc_connectivity,
-        input_connectivity=input_connectivity,
-        fb_connectivity=fb_connectivity,
+        UNITS,
+        lr=LEAK_RATE,
+        sr=RHO,
+        input_bias=INPUT_BIAS,
+        input_scaling=INPUT_SCALING,
+        rc_connectivity=RC_CONNECTIVITY,
+        input_connectivity=INPUT_CONNECTIVITY,
+        fb_connectivity=FB_CONNECTIVITY,
         name="reservoir",
     )
 
     # If you want to use custom matrices, then use:
     custom_reservoir = Reservoir(
-        units,
-        lr=leak_rate,
-        input_bias=input_bias,
         W=W,
+        lr=LEAK_RATE,
+        input_bias=INPUT_BIAS,
         Win=Win,
         Wfb=Wfb,
         name="custom-reservoir",
     )
 
     # create a readout layer equipped with an offline learning rule
-    readout = Ridge(ridge=regularization_coef, name="readout")
+    readout = Ridge(ridge=REGULARIZATION_COEF, name="readout")
 
-    if feedback:
+    if FEEDBACK:
         reservoir = reservoir << readout
 
-    esn = reservoir >> readout
-
-    # (optional) Connect the inputs directly to the readout
-    # inputs = Input(name="input")
-    # esn = [inputs >> reservoir, inputs] >> readout
+    if INPUT_TO_READOUT:
+        # Connect the inputs directly to the readout
+        inputs = Input(name="input")
+        esn = [inputs >> reservoir, inputs] >> readout
+    else:
+        esn = reservoir >> readout
 
     # ---- Let's have a look at internal states ----
 
@@ -251,7 +268,7 @@ if __name__ == "__main__":
 
     # ---- Train the ESN ----
 
-    esn = esn.fit(X, y, warmup=warmup)
+    esn = esn.fit(X, y, warmup=WARMUP)
 
     # ---- Evaluate the ESN ----
 
@@ -274,7 +291,7 @@ if __name__ == "__main__":
     nmrse_maxmin = nrmse(y_test, y_pred, norm_value=y.max() - y.min())
 
     print("\n********************")
-    print(f"Errors computed over {test_size} time steps")
+    print(f"Errors computed over {TEST_SIZE} time steps")
     print("\nMean Squared error (MSE):\t%.4e" % mse_score)
     print("Root Mean Squared error (RMSE):\t%.4e" % rmse_score)
     print("Normalized RMSE (based on mean):\t%.4e" % nmrse_mean)
