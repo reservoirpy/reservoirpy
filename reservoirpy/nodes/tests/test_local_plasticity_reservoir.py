f2295e6c076d7b5f4f500782e8535c2ddd1dc933
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-04-02 20:18:52 +0200

    LPR coverage

--- reservoirpy/nodes/tests/test_local_plasticity_reservoir.py
+++ reservoirpy/nodes/tests/test_local_plasticity_reservoir.py
@@ -32,15 +32,15 @@ def test_lsp_rules():
     x = np.ones((10, 5))
 
     res = LocalPlasticityReservoir(100, local_rule="oja")
-    _ = res.run(x)
+    _ = res.fit(x)
     res = LocalPlasticityReservoir(100, local_rule="anti-oja")
-    _ = res.run(x)
+    _ = res.fit(x)
     res = LocalPlasticityReservoir(100, local_rule="hebbian")
-    _ = res.run(x)
+    _ = res.fit(x)
     res = LocalPlasticityReservoir(100, local_rule="anti-hebbian")
-    _ = res.run(x)
+    _ = res.fit(x)
     res = LocalPlasticityReservoir(100, local_rule="bcm")
-    _ = res.run(x)
+    _ = res.fit(x)
 
     with pytest.raises(ValueError):
         res = LocalPlasticityReservoir(100, local_rule="anti-bcm")
@@ -62,7 +62,7 @@ def test_local_synaptic_plasticity():
     assert res.W.shape == (100, 100)
 
     res = LocalPlasticityReservoir(
-        100, local_rule="oja", epochs=10, eta=1e-3, synapse_normalization=False
+        100, local_rule="oja", epochs=10, eta=1e-3, synapse_normalization=True
     )
     res.initialize(x)
 
@@ -102,14 +102,18 @@ def test_lsp_model():
 def test_lsp_matrices():
     rng = np.random.default_rng(seed=2504)
     x = rng.normal(size=(100, 5))
-    y = rng.normal(size=(100, 2))
 
     W = ring(10, 10)
 
     lspres_ring = LocalPlasticityReservoir(W=W, seed=2504)
     lspres_rand = LocalPlasticityReservoir(units=10, seed=2504)
 
-    lspres_ring.fit(x, y)
-    lspres_rand.fit(x, y)
+    lspres_ring.fit(x)
+    lspres_rand.fit(x)
 
     assert not np.allclose(lspres_ring.W.toarray(), lspres_rand.W.toarray())
+
+    # test dense matrix
+    W = rng.normal(size=(10, 10))
+    res = LocalPlasticityReservoir(W=W)
+    res.fit(x)
