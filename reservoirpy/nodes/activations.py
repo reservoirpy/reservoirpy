bf5db657a206aa79472d8842a728a1cd6dbd30d2
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-04-07 14:10:41 +0200

    v0.4 activation nodes

--- reservoirpy/nodes/activations.py
+++ reservoirpy/nodes/activations.py
@@ -2,153 +2,90 @@
 # Licence: MIT License
 # Copyright: Xavier Hinaut (2018) <xavier.hinaut@inria.fr>
 from functools import partial
+from typing import Callable, Optional, Tuple
 
-from ..activationsfunc import get_function
+from ..activationsfunc import identity, relu, sigmoid, softmax, softplus, tanh
 from ..node import Node
+from ..type import NodeInput, Timeseries, Timestep
 
 
-def forward(node: Node, x, **kwargs):
-    return node.f(x, **kwargs)
+class F(Node):
+    def __init__(self, f: Callable, **kwargs):
+        self.f = partial(f, **kwargs)
+        self.initialized = False
+
+    def initialize(
+        self, x: Optional[NodeInput | Timestep], y: Optional[NodeInput | Timestep]
+    ):
+        self.input_dim = x.shape[-1] if not isinstance(x, list) else x[0].shape[-1]
+        self.output_dim = self.input_dim
+        self.initialized = True
+
+    def _step(self, state: tuple, x: Timestep) -> Tuple[tuple, Timestep]:
+        return (), self.f(x)
+
+    def _run(self, state: tuple, x: Timeseries) -> Tuple[tuple, Timeseries]:
+        return (), self.f(x)
 
 
-def initialize(node: Node, x=None, *args, **kwargs):
-    if x is not None:
-        node.input_dim = x.shape[1]
-        node.input_dim = x.shape[1]
-
-
-class Softmax(Node):
+class Softmax(F):
     """Softmax activation function.
 
     .. math::
 
         y_k = \\frac{e^{\\beta x_k}}{\\sum_{i=1}^{n} e^{\\beta x_i}}
 
-    :py:attr:`Softmax.hypers` **list**
-
-    ============= ======================================================================
-    ``f``         Activation function (:py:func:`reservoir.activationsfunc.softmax`).
-    ``beta``      Softmax :math:`\\beta` parameter (1.0 by default).
-    ============= ======================================================================
-
     Parameters
     ----------
     beta: float, default to 1.0
         Beta parameter of softmax.
-    input_dim : int, optional
-        Input dimension. Can be inferred at first call.
-    name : str, optional
-        Node name.
-    dtype : Numpy dtype, default to np.float64
-        Numerical type for node parameters.
     """
 
-    def __init__(self, beta=1.0, **kwargs):
-        super(Softmax, self).__init__(
-            hypers={"f": get_function("softmax"), "beta": beta},
-            forward=partial(forward, beta=beta),
-            initializer=initialize,
-            **kwargs,
-        )
+    def __init__(self, beta=1.0):
+        self.f = partial(softmax, beta=beta)
+        self.initialized = False
 
 
-class Softplus(Node):
+class Softplus(F):
     """Softplus activation function.
 
     .. math::
 
         f(x) = \\mathrm{ln}(1 + e^{x})
-
-    :py:attr:`Softplus.hypers` **list**
-
-    ============= ======================================================================
-    ``f``         Activation function (:py:func:`reservoir.activationsfunc.softplus`).
-    ============= ======================================================================
-
-    Parameters
-    ----------
-    input_dim : int, optional
-        Input dimension. Can be inferred at first call.
-    name : str, optional
-        Node name.
-    dtype : Numpy dtype, default to np.float64
-        Numerical type for node parameters.
     """
 
-    def __init__(self, **kwargs):
-        super(Softplus, self).__init__(
-            hypers={"f": get_function("softplus")},
-            forward=forward,
-            initializer=initialize,
-            **kwargs,
-        )
+    def __init__(self):
+        self.f = softplus
+        self.initialized = False
 
 
-class Sigmoid(Node):
+class Sigmoid(F):
     """Sigmoid activation function.
 
     .. math::
 
         f(x) = \\frac{1}{1 + e^{-x}}
-
-    :py:attr:`Sigmoid.hypers` **list**
-
-    ============= ======================================================================
-    ``f``         Activation function (:py:func:`reservoir.activationsfunc.sigmoid`).
-    ============= ======================================================================
-
-    Parameters
-    ----------
-    input_dim : int, optional
-        Input dimension. Can be inferred at first call.
-    name : str, optional
-        Node name.
-    dtype : Numpy dtype, default to np.float64
-        Numerical type for node parameters.
     """
 
-    def __init__(self, **kwargs):
-        super(Sigmoid, self).__init__(
-            hypers={"f": get_function("sigmoid")},
-            forward=forward,
-            initializer=initialize,
-            **kwargs,
-        )
+    def __init__(self):
+        self.f = sigmoid
+        self.initialized = False
 
 
-class Tanh(Node):
+class Tanh(F):
     """Hyperbolic tangent activation function.
 
     .. math::
 
         f(x) = \\frac{e^x - e^{-x}}{e^x + e^{-x}}
-
-    :py:attr:`Tanh.hypers` **list**
-
-    ============= ======================================================================
-    ``f``         Activation function (:py:func:`reservoir.activationsfunc.tanh`).
-    ============= ======================================================================
-
-    Parameters
-    ----------
-    input_dim : int, optional
-        Input dimension. Can be inferred at first call.
-    name : str, optional
-        Node name.
-    dtype : Numpy dtype, default to np.float64
-        Numerical type for node parameters.
     """
 
-    def __init__(self, **kwargs):
-        super(Tanh, self).__init__(
-            hypers={"f": get_function("tanh")},
-            forward=forward,
-            initializer=initialize,
-            **kwargs,
-        )
+    def __init__(self):
+        self.f = tanh
+        self.initialized = False
 
 
-class Identity(Node):
+class Identity(F):
     """Identity function.
 
     .. math::
@@ -156,59 +93,21 @@ class Identity(Node):
         f(x) = x
 
     Provided for convenience.
-
-    :py:attr:`Identity.hypers` **list**
-
-    ============= ======================================================================
-    ``f``         Activation function (:py:func:`reservoir.activationsfunc.identity`).
-    ============= ======================================================================
-
-    Parameters
-    ----------
-    input_dim : int, optional
-        Input dimension. Can be inferred at first call.
-    name : str, optional
-        Node name.
-    dtype : Numpy dtype, default to np.float64
-        Numerical type for node parameters.
     """
 
-    def __init__(self, **kwargs):
-        super(Identity, self).__init__(
-            hypers={"f": get_function("identity")},
-            forward=forward,
-            initializer=initialize,
-            **kwargs,
-        )
+    def __init__(self):
+        self.f = identity
+        self.initialized = False
 
 
-class ReLU(Node):
+class ReLU(F):
     """ReLU activation function.
 
     .. math::
 
         f(x) = x ~~ \\mathrm{if} ~~ x > 0 ~~ \\mathrm{else} ~~ 0
-
-    :py:attr:`ReLU.hypers` **list**
-
-    ============= ======================================================================
-    ``f``         Activation function (:py:func:`reservoir.activationsfunc.relu`).
-    ============= ======================================================================
-
-    Parameters
-    ----------
-    input_dim : int, optional
-        Input dimension. Can be inferred at first call.
-    name : str, optional
-        Node name.
-    dtype : Numpy dtype, default to np.float64
-        Numerical type for node parameters.
     """
 
-    def __init__(self, **kwargs):
-        super(ReLU, self).__init__(
-            hypers={"f": get_function("relu")},
-            forward=forward,
-            initializer=initialize,
-            **kwargs,
-        )
+    def __init__(self):
+        self.f = relu
+        self.initialized = False
