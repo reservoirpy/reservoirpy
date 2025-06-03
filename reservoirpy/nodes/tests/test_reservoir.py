75b718bcc89c0a9fedb1bdac2fd705e2b0261378
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-06-03 15:47:30 +0200

    updated tests for v0.4 Reservoir node

--- reservoirpy/nodes/tests/test_reservoir.py
+++ reservoirpy/nodes/tests/test_reservoir.py
@@ -6,13 +6,14 @@ import pytest
 from numpy.testing import assert_array_equal
 
 from reservoirpy.activationsfunc import relu
+from reservoirpy.mat_gen import bernoulli, zeros
 from reservoirpy.nodes import Reservoir
 
 
 def test_reservoir_init():
-    node = Reservoir(100, lr=0.8, input_bias=False)
+    node = Reservoir(100, lr=0.8, bias=zeros)
 
-    data = np.ones((1, 10))
+    data = np.ones((10,))
     res = node(data)
 
     assert node.W.shape == (100, 100)
@@ -27,9 +28,6 @@ def test_reservoir_init():
 
     with pytest.raises(ValueError):
         Reservoir()
-
-    with pytest.raises(ValueError):
-        Reservoir(100, equation="foo")
 
     res = Reservoir(100, activation="relu")
     assert id(res.activation) == id(relu)
@@ -52,9 +50,9 @@ def test_reservoir_init_from_lr_is_arrays():
 def test_reservoir_init_from_matrices():
     Win = np.ones((100, 10))
 
-    node = Reservoir(100, lr=0.8, Win=Win, input_bias=False)
+    node = Reservoir(100, lr=0.8, Win=Win, bias=bernoulli)
 
-    data = np.ones((1, 10))
+    data = np.ones((10,))
     res = node(data)
 
     assert node.W.shape == (100, 100)
@@ -67,30 +65,29 @@ def test_reservoir_init_from_matrices():
 
     assert res.shape == (10000, 100)
 
-    Win = np.ones((100, 11))
+    Win = np.ones((100, 10))
+    bias = np.ones((100,))
 
-    node = Reservoir(100, lr=0.8, Win=Win, input_bias=True)
+    node = Reservoir(100, lr=0.8, Win=Win, bias=bias)
 
-    data = np.ones((1, 10))
+    data = np.ones((10,))
     res = node(data)
 
     assert node.W.shape == (100, 100)
-    assert_array_equal(np.c_[node.bias, node.Win], Win)
     assert node.lr == 0.8
     assert node.units == 100
+    assert node.output_dim == 100
 
     data = np.ones((10000, 10))
     res = node.run(data)
 
     assert res.shape == (10000, 100)
 
-    # Shape override (matrix.shape > units parameter)
-    data = np.ones((1, 10))
-    W = np.ones((10, 10))
-    res = Reservoir(100, W=W)
-    _ = res(data)
-    assert res.units == 10
-    assert res.output_dim == 10
+    with pytest.raises(ValueError):  # Shape override (matrix.shape > units parameter)
+        data = np.ones((10))
+        W = np.ones((10, 10))
+        res = Reservoir(100, W=W)
+        _ = res(data)
 
     with pytest.raises(ValueError):  # Bad matrix shape
         W = np.ones((10, 11))
@@ -101,16 +98,12 @@ def test_reservoir_init_from_matrices():
         res = Reservoir(100, W=1.0)
         res(data)
 
-    with pytest.raises(ValueError):  # Bias in Win but no bias accepted
-        res = Reservoir(100, Win=np.ones((100, 11)), input_bias=False)
+    with pytest.raises(ValueError):  # Bad Win shape
+        res = Reservoir(100, Win=np.ones((100, 20)))
         res(data)
 
     with pytest.raises(ValueError):  # Bad Win shape
-        res = Reservoir(100, Win=np.ones((100, 20)), input_bias=True)
-        res(data)
-
-    with pytest.raises(ValueError):  # Bad Win shape
-        res = Reservoir(100, Win=np.ones((101, 10)), input_bias=True)
+        res = Reservoir(100, Win=np.ones((101, 10)))
         res(data)
 
     with pytest.raises(ValueError):  # Bad matrix format
@@ -119,141 +112,81 @@ def test_reservoir_init_from_matrices():
 
 
 def test_reservoir_bias():
-    node = Reservoir(100, lr=0.8, input_bias=False)
-
-    data = np.ones((1, 10))
+    node = Reservoir(100, lr=0.8, bias=zeros)
+    data = np.ones((10,))
     res = node(data)
-
     assert node.W.shape == (100, 100)
     assert node.Win.shape == (100, 10)
-    assert node.bias.shape == (100, 1)
-    assert_array_equal(node.bias, np.zeros((100, 1)))
+    assert node.bias.shape == (100,)
+    assert_array_equal(node.bias, np.zeros((100,)))
     assert node.lr == 0.8
     assert node.units == 100
 
-    node = Reservoir(100, lr=0.8, input_bias=True)
-
-    data = np.ones((1, 10))
+    node = Reservoir(100, lr=0.8, bias=bernoulli)
+    data = np.ones((10,))
     res = node(data)
+    assert node.bias.shape == (100,)
 
-    assert node.bias.shape == (100, 1)
-
-    bias = np.ones((100, 1))
+    bias = np.ones((100,))
     node = Reservoir(100, bias=bias)
     res = node(data)
-
     assert_array_equal(node.bias, bias)
 
     bias = np.ones((100,))
     node = Reservoir(100, bias=bias)
     res = node(data)
-
     assert_array_equal(node.bias, bias)
 
-    with pytest.raises(ValueError):
-        bias = np.ones((101, 1))
+    data = np.zeros((100,))
+    node = Reservoir(100, bias=1.0)
+    res = node(data)
+    assert_array_equal(res, np.tanh(np.ones((100,))))
+
+    with pytest.raises(AssertionError):
+        bias = np.ones((100, 1))
         node = Reservoir(100, bias=bias)
-        node(data)
+        res = node(data)
+        assert res.shape == (100,)
 
     with pytest.raises(ValueError):
-        bias = np.ones((101, 2))
+        bias = np.ones((100, 2))
         node = Reservoir(100, bias=bias)
-        node(data)
-
-    with pytest.raises(ValueError):
-        node = Reservoir(100, bias=1.0)
         node(data)
 
 
 def test_reservoir_run():
     x = np.ones((10, 5))
 
-    res = Reservoir(100, equation="internal")
-    out = res.run(x)
-    assert out.shape == (10, 100)
-
-    res = Reservoir(100, equation="external")
+    res = Reservoir(100)
     out = res.run(x)
     assert out.shape == (10, 100)
 
 
-def test_reservoir_chain():
-    node1 = Reservoir(100, lr=0.8, input_bias=False)
-    node2 = Reservoir(50, lr=1.0, input_bias=False)
+# def test_reservoir_chain():
+#     node1 = Reservoir(100, lr=0.8, input_bias=False)
+#     node2 = Reservoir(50, lr=1.0, input_bias=False)
 
-    data = np.ones((1, 10))
-    res = (node1 >> node2)(data)
+#     data = np.ones((1, 10))
+#     res = (node1 >> node2)(data)
 
-    assert node1.W.shape == (100, 100)
-    assert node1.Win.shape == (100, 10)
-    assert node2.W.shape == (50, 50)
-    assert node2.Win.shape == (50, 100)
+#     assert node1.W.shape == (100, 100)
+#     assert node1.Win.shape == (100, 10)
+#     assert node2.W.shape == (50, 50)
+#     assert node2.Win.shape == (50, 100)
 
-    assert res.shape == (1, 50)
-
-
-def test_reservoir_feedback():
-    node1 = Reservoir(100, lr=0.8, input_bias=False)
-    node2 = Reservoir(50, lr=1.0, input_bias=False)
-
-    node1 <<= node2
-
-    data = np.ones((1, 10))
-    res = (node1 >> node2)(data)
-
-    assert node1.W.shape == (100, 100)
-    assert node1.Win.shape == (100, 10)
-    assert node2.W.shape == (50, 50)
-    assert node2.Win.shape == (50, 100)
-
-    assert res.shape == (1, 50)
-
-    with pytest.raises(ValueError):
-        node1 = Reservoir(100, lr=0.8)
-        node2 = Reservoir(50, lr=1.0)
-        node1 <<= node2
-        data = np.ones((1, 10))
-        res = (node1 >> node2)(data)
-
-    with pytest.raises(ValueError):
-        node1 = Reservoir(100, lr=0.8)
-        node2 = Reservoir(50, lr=1.0)
-        node1 <<= node2
-        data = np.ones((1, 10))
-        res = (node1 >> node2)(data)
-
-    with pytest.raises(ValueError):
-        node1 = Reservoir(100, lr=0.8)
-        node2 = Reservoir(50, lr=1.0)
-        node1 <<= node2
-        data = np.ones((1, 10))
-        res = (node1 >> node2)(data)
+#     assert res.shape == (1, 50)
 
 
-def test_reservoir_noise():
-    node1 = Reservoir(100, seed=123, noise_rc=0.1, noise_in=0.5)
-    node2 = Reservoir(100, seed=123, noise_rc=0.1, noise_in=0.5)
+def test_reservoir_seed():
+    node1 = Reservoir(100, seed=123)
+    node2 = Reservoir(100, seed=123)
 
     data = np.ones((10, 10))
 
     assert_array_equal(node1.run(data), node2.run(data))
 
-    node1 = Reservoir(
-        100,
-        seed=123,
-        noise_rc=0.1,
-        noise_in=0.5,
-        noise_type="uniform",
-        noise_kwargs={"low": -1, "high": 0.5},
-    )
-    node2 = Reservoir(
-        100,
-        seed=123,
-        noise_rc=0.1,
-        noise_in=0.5,
-        noise_type="uniform",
-        noise_kwargs={"low": -1, "high": 0.5},
-    )
+    node1 = Reservoir(100, seed=123)
+    node2 = Reservoir(100, seed=123)
 
     data = np.ones((10, 10))
 
