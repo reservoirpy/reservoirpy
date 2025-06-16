2db11624afb191d93cb7234292a1ff97a9e90e8a
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-06-16 17:18:35 +0200

    v0.4 activations function tests

--- reservoirpy/nodes/tests/test_activations.py
+++ reservoirpy/nodes/tests/test_activations.py
@@ -11,7 +11,34 @@ from reservoirpy.nodes import Identity, ReLU, Sigmoid, Softmax, Softplus, Tanh
     "node",
     (Tanh(), Softmax(), Softplus(), Sigmoid(), Identity(), ReLU(), Softmax(beta=2.0)),
 )
-def test_activation(node):
-    x = np.ones((1, 10))
+def test_activation_step(node):
+    x = np.ones((10,))
     out = node(x)
     assert out.shape == x.shape
+    assert node.input_dim == x.shape[-1]
+    assert node.output_dim == x.shape[-1]
+
+
+@pytest.mark.parametrize(
+    "node",
+    (Tanh(), Softmax(), Softplus(), Sigmoid(), Identity(), ReLU(), Softmax(beta=2.0)),
+)
+def test_activation_run(node):
+    x = np.ones((100, 10))
+    out = node.run(x)
+    assert out.shape == x.shape
+    assert node.input_dim == x.shape[-1]
+    assert node.output_dim == x.shape[-1]
+
+    out2 = node.step(x[-1])
+    np.testing.assert_array_almost_equal(out[-1], out2)
+
+    X = list(np.ones((7, 100, 10)))
+    out = node.run(X)
+    assert isinstance(out, list)
+    assert out[0].shape == X[0].shape
+    assert node.input_dim == X[0].shape[-1]
+    assert node.output_dim == X[0].shape[-1]
+
+    out2 = node.step(X[0][-1])
+    np.testing.assert_array_almost_equal(out[0][-1], out2)
