eab795f08a36ca39a70365e8dbbd74e284cf1eac
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-07-26 17:44:00 +0200

    Remove nodes.reservoirs submodule

--- reservoirpy/nodes/reservoirs/__init__.py
+++
@@ -1,9 +0,0 @@
-# Author: Nathan Trouvain at 08/03/2022 <nathan.trouvain@inria.fr>
-# Licence: MIT License
-# Copyright: Xavier Hinaut (2018) <xavier.hinaut@inria.fr>
-from .intrinsic_plasticity import IPReservoir
-from .local_plasticity_reservoir import LocalPlasticityReservoir
-from .nvar import NVAR
-from .reservoir import Reservoir
-
-__all__ = ["Reservoir", "IPReservoir", "NVAR", "LocalPlasticityReservoir"]
