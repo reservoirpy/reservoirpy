34ae35fd9c165755387d20624c3d7a125df2c67d
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-07-28 18:50:33 +0200

    Removed unused utils.parallel & utils.validation

--- reservoirpy/utils/__init__.py
+++ reservoirpy/utils/__init__.py
@@ -1,11 +1,3 @@
 # Author: Nathan Trouvain at 19/06/2021 <nathan.trouvain@inria.fr>
 # Licence: MIT License
 # Copyright: Xavier Hinaut (2018) <xavier.hinaut@inria.fr>
-from inspect import signature
-
-
-def _obj_from_kwargs(klas, kwargs):
-    sig = signature(klas.__init__)
-    params = list(sig.parameters.keys())
-    klas_kwargs = {n: v for n, v in kwargs.items() if n in params}
-    return klas(**klas_kwargs)
