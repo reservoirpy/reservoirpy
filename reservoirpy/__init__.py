7f0ed053596ec93406b1d0c0732d699da40f929e
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-08-12 17:48:30 +0200

    Uniquely identify tmp folders

--- reservoirpy/__init__.py
+++ reservoirpy/__init__.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+import uuid
 
 from . import activationsfunc, hyper, mat_gen, nodes, observables, type
 from ._version import __version__
@@ -7,7 +8,7 @@ from .model import Model
 from .node import Node
 from .utils.random import set_seed
 
-_TEMPDIR = os.path.join(tempfile.gettempdir(), "reservoirpy-temp")
+_TEMPDIR = os.path.join(tempfile.gettempdir(), f"reservoirpy-temp-{uuid.uuid4()}")
 if not os.path.exists(_TEMPDIR):  # pragma: no cover
     try:
         os.mkdir(_TEMPDIR)
