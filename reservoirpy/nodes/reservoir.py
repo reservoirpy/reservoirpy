1bf62766d45fbb1a5f07542e4ae30a91609dfd84
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-08-13 16:01:00 +0200

    Doc: Reservoir

--- reservoirpy/nodes/reservoir.py
+++ reservoirpy/nodes/reservoir.py
@@ -10,25 +10,138 @@ from ..utils import random
 
 
 class Reservoir(Node):
+    """Pool of leaky-integrator neurons with random recurrent connexions.
+
+    Reservoir neurons states, gathered in a vector :math:`\\mathbf{x}`, follow
+    the update rule below:
+
+    .. math::
+
+        \\mathbf{x}[t+1] = (1 - \\mathrm{lr}) * \\mathbf{x}[t] + \\mathrm{lr}
+         * f(\\mathbf{W}_{in} \\cdot (\\mathbf{u}[t+1])
+          + \\mathbf{W} \\cdot \\mathbf{x}[t])
+
+    where:
+        - :math:`\\mathbf{x}` is the output activation vector of the reservoir;
+        - :math:`\\mathbf{u}` is the input timeseries;
+        - :math:`f` and :math:`g` are activation functions.
+
+
+    Parameters
+    ----------
+    units : int, optional
+        Number of reservoir units. If None, the number of units will be inferred from
+        the ``W`` matrix shape.
+    lr : float or array-like of shape (units,), default to 1.0
+        Neurons leak rate. Must be in :math:`[0, 1]`.
+    sr : float, optional
+        Spectral radius of recurrent weight matrix.
+    input_scaling : float or array-like of shape (features,), default to 1.0.
+        Input gain. An array of the same dimension as the inputs can be used to
+        set up different input scaling for each feature.
+    input_connectivity : float, default to 0.1
+        Connectivity of input neurons, i.e. ratio of input neurons connected
+        to reservoir neurons. Must be in :math:`]0, 1]`.
+    rc_connectivity : float, default to 0.1
+        Connectivity of recurrent weight matrix, i.e. ratio of reservoir
+        neurons connected to other reservoir neurons, including themselves.
+        Must be in :math:`]0, 1]`.
+    Win : callable or array-like of shape (units, features), default to :py:func:`~reservoirpy.mat_gen.bernoulli`
+        Input weights matrix or initializer. If a callable (like a function) is used,
+        then this function should accept any keywords
+        parameters and at least two parameters that will be used to define the shape of
+        the returned weight matrix.
+    W : callable or array-like of shape (units, units), default to :py:func:`~reservoirpy.mat_gen.normal`
+        Recurrent weights matrix or initializer. If a callable (like a function) is
+        used, then this function should accept any keywords
+        parameters and at least two parameters that will be used to define the shape of
+        the returned weight matrix.
+    bias : callable or array-like of shape (units, 1), default to :py:func:`~reservoirpy.mat_gen.bernoulli`
+        Bias weights vector or initializer. If a callable (like a function) is
+        used, then this function should accept any keywords
+        parameters and at least two parameters that will be used to define the shape of
+        the returned weight matrix.
+    activation : str or callable, default to :py:func:`~reservoirpy.activationsfunc.tanh`
+        Reservoir units activation function.
+        - If a str, should be a :py:mod:`~reservoirpy.activationsfunc`
+        function name.
+        - If a callable, should be an element-wise operator on arrays.
+    input_dim : int, optional
+        Input dimension. Can be inferred at first call.
+    dtype : Numpy dtype, default to np.float64
+        Numerical type for node parameters.
+    seed : int or :py:class:`numpy.random.Generator`, optional
+        A random state seed, for noise generation.
+    name : str, optional
+        Node name.
+
+    Note
+    ----
+
+    If W, Win, bias or Wfb are initialized with an array-like matrix, then all
+    initializers parameters such as spectral radius (``sr``) or input scaling
+    (``input_scaling``) are ignored.
+    See :py:mod:`~reservoirpy.mat_gen` for more information.
+
+    Example
+    -------
+
+    >>> from reservoirpy.nodes import Reservoir
+    >>> reservoir = Reservoir(100, lr=0.2, sr=0.8) # a 100 neurons reservoir
+
+    Using the :py:func:`~reservoirpy.datasets.mackey_glass` timeseries:
+
+    >>> from reservoirpy.datasets import mackey_glass
+    >>> x = mackey_glass(200)
+    >>> states = reservoir.run(x)
+
+    .. plot::
+
+        from reservoirpy.nodes import Reservoir
+        reservoir = Reservoir(100, lr=0.2, sr=0.8)
+        from reservoirpy.datasets import mackey_glass
+        x = mackey_glass(200)
+        states = reservoir.run(x)
+        fig, ax = plt.subplots(6, 1, figsize=(7, 10), sharex=True)
+        ax[0].plot(x)
+        ax[0].grid()
+        ax[0].set_title("Neuron states (on Mackey-Glass)")
+        for i in range(1, 6):
+            ax[i].plot(states[:, i], label=f"Neuron {i}")
+            ax[i].legend()
+            ax[i].grid()
+        ax[-1].set_xlabel("Timesteps")
+    """
+
     initialized: bool
     input_dim: Optional[int]
     output_dim: int
+    name: Optional[str]
+    state: State
 
     # params
+    #: Number of neuronal units in the reservoir.
     units: int
+    #: Leaking rate (1.0 by default) (:math:`\\mathrm{lr}`).
     lr: Union[float, np.ndarray]
+    #: Spectral radius of ``W`` (optional).
     sr: float
+    #: Input scaling (float or array) (1.0 by default).
     input_scaling: Union[float, Sequence]
+    #: Connectivity (or density) of ``Win`` (0.1 by default).
     input_connectivity: float
+    #: Connectivity (or density) of ``Wfb`` (0.1 by default).
     rc_connectivity: float
+    #: Input weights matrix (:math:`\\mathbf{W}_{in}`).
     Win: Union[Weights, Callable]
+    #: Recurrent weights matrix (:math:`\\mathbf{W}`).
     W: Union[Weights, Callable]
+    #: Bias vector (:math:`\\mathbf{b}`).
     bias: Union[Weights, Callable, float]
+    #: Activation of the reservoir units (tanh by default) (:math:`f`).
     activation: Callable
+    #: A random state generator. Used for generating Win and W.
     rng: np.random.Generator
-    name: Optional[str]
-    # state
-    state: State
 
     def __init__(
         self,
