9e5d9616fc7a2df1aa01e0935f3ccdd0f61f630d
Romain de Coudenhove <decoudenhove.romain@gmail.com>
2025-08-13 23:06:12 +0200

    Add function cpu_parallelized_hypersearch to the hyperparameter optimization module

--- reservoirpy/hyper/__init__.py
+++ reservoirpy/hyper/__init__.py
@@ -3,8 +3,8 @@
 Hyperparameter optimization helpers (:mod:`reservoirpy.hyper`)
 ==============================================================
 
-Utility module for optimization of hyperparameters
-using *hyperopt*.
+Utility module for optimizing hyperparameters, with support for both
+*hyperopt*-based search and CPU-parallelized custom search using *joblib*.
 
 Note
 ----
@@ -15,14 +15,19 @@ with ReservoirPy by default. Before using the
 :py:mod:`reservoirpy.hyper` module, consider installing
 these packages if they are not already installed.
 
+The function `cpu_parallelized_research` of this module
+do not rely on *hyperopt* and can be used independently.
+
 
 .. autosummary::
     :toctree: generated/
 
     research
+    cpu_parallelized_research
     plot_hyperopt_report
 """
 from ._hyperplot import plot_hyperopt_report
 from ._hypersearch import research
+from ._cpu_parallelized_hypersearch import cpu_parallelized_research
 
-__all__ = ["research", "plot_hyperopt_report"]
+__all__ = ["research", "cpu_parallelized_research", "plot_hyperopt_report"]
