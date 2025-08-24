9d91f59283f8301ee1db854d1fe93aada6a304b4
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-08-24 16:38:28 +0200

    Better repr for Nodes and Models

--- reservoirpy/utils/__init__.py
+++ reservoirpy/utils/__init__.py
@@ -1,3 +1,35 @@
 # Author: Nathan Trouvain at 19/06/2021 <nathan.trouvain@inria.fr>
 # Licence: MIT License
 # Copyright: Xavier Hinaut (2018) <xavier.hinaut@inria.fr>
+
+EXCLUDED_PARAMETERS = ["Win", "W", "bias", "Wout"]
+
+
+def get_non_defaults(instance, constructor=None):
+    """
+    Return a dictionary of key: values where the key is both an attribute of ``instance`` and a named parameter of its
+    constructor (or a parameter of ``constructor`` if specified), but the attribute value is different from the default
+    value of the constructor. For the sake of readability, some parameters names are excluded, such as ``W`` or ``bias``.
+
+    This method is used to ``__repr__``.
+
+    Parameters
+    ----------
+    instance : object
+    constructor : class, optional
+
+    Returns
+    -------
+    dict[str, Any]
+    """
+    if constructor is None:
+        constructor = type(instance)
+    import inspect
+
+    signature = inspect.signature(constructor)
+    defaults = {k: v.default for k, v in signature.parameters.items() if v.default is not inspect.Parameter.empty}
+    return {
+        k: getattr(instance, k)
+        for k, v in defaults.items()
+        if hasattr(instance, k) and getattr(instance, k) != v and k not in EXCLUDED_PARAMETERS
+    }
