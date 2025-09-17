9a0591883a6f507bbb3e8726fa8efc765829618d
PAUL BERNARD <Paul.Bernard@inria.fr>
2025-09-17 19:25:41 +0200

    jax.Node

--- reservoirpy/jax/__init__.py
+++ reservoirpy/jax/__init__.py
@@ -1,7 +1,7 @@
 # Licence: MIT License
 # Copyright: Xavier Hinaut (2018) <xavier.hinaut@inria.fr>
-# from .node import Node
 from . import activationsfunc, mat_gen
+from .node import Node
 
 __all__ = [
     "mat_gen",
@@ -9,7 +9,7 @@ __all__ = [
     # "observables",  # NOT PLANNED FOR NOW
     # "hyper",  # NOT PLANNED
     # "nodes",  # WIP
-    # "Node",  # WIP
+    "Node",
     # "Model",  # WIP
     # "ESN",  # WIP
     # "type",  # We use reservoirpy.type instead, at least for now
